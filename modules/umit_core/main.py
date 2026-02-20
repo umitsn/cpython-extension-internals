de82669b8a32c0f21beb48fc97102f41379fba9c
Umit Sen <umit.sn@hotmail.com>
2026-02-20 21:52:33 +0300

    METH_NOARGS & Py_UNUSED example, test main.py

--- modules/umit_core/main.py
+++ modules/umit_core/main.py
@@ -1,6 +1,13 @@
 import umit_core as umit
 
+print("umt_selam fonksiyonu")
 umit.umt_selam()
 umit.umt_selam("ümit")
 
-help(umit)
+# help(umit)
+
+print("\numt_selam2 fonksiyonu")
+umit.umt_selam2()
+
+print("\numt_selam2 fonksiyonu")
+umit.umt_selam2("ümit")
