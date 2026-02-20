e2bc8f2157157ee43ac0f030a53f10b417643639
Umit Sen <umit.sn@hotmail.com>
2026-02-20 21:56:44 +0300

    METH_NOARGS & Py_UNUSED example

--- modules/umit_core/umit_core.c
+++ modules/umit_core/umit_core.c
@@ -5,8 +5,16 @@ static PyObject* method_selam(PyObject* self, PyObject* args){
     Py_RETURN_NONE;
 }
 
+static PyObject* method_selam2(PyObject* self, PyObject* Py_UNUSED(args)){
+    printf("Py_UNUSED ve METH_NOARGS yapısı / ilişkisi\n");
+    printf("Selam Ben Ümit ŞEN, umt_selam2 Fonksiyonu C Katmanında Başarılı Şekilde Çalıştı. :)))\n");
+    Py_RETURN_NONE;
+}
+
+
 static PyMethodDef UmitMethods[] = {
     {"umt_selam",method_selam,METH_VARARGS, "C dili özel selam."},
+    {"umt_selam2",method_selam2,METH_NOARGS, "umt_selam2 fonksiyonu help kısmı."},
     {NULL,NULL,0,NULL}
 };
 
