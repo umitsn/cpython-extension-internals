2a33f4b23625d3d47f41877613b22ce6884cc042
Ümit Şen <49841290+umitsn@users.noreply.github.com>
2026-05-04 11:23:26 +0300

    Add files via upload

---
+++ lessons/004-PyArg_ParseTuple-string-parse/main.py
@@ -0,0 +1,28 @@
+import umit_core as umit
+
+# print("umt_selam fonksiyonu")
+# umit.umt_selam()
+# umit.umt_selam("ümit")
+
+# # help(umit)
+
+# print("\numt_selam2 fonksiyonu")
+# umit.umt_selam2()
+
+# print("\numt_selam2 fonksiyonu")
+# umit.umt_selam2("ümit")
+
+print("\numt_selam3 fonksiyonu")
+umit.umt_selam3("ümit")
+"""
+umt_selam3 fonksiyonu
+Selam ümit,artık C katmanındasın
+"""
+
+
+# print("\numt_selam3 fonksiyonu")
+# umit.umt_selam3(123)
+"""
+umit.umt_selam3(123)
+TypeError: argument 1 must be str, not int
+"""
