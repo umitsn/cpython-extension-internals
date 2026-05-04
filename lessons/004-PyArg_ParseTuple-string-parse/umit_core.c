7bc4e81d633057f03d144cfa80a809681dd1dfe6
Ümit Şen <49841290+umitsn@users.noreply.github.com>
2026-05-04 11:17:16 +0300

    Add files via upload

---
+++ lessons/004-PyArg_ParseTuple-string-parse/umit_core.c
@@ -0,0 +1,41 @@
+#include <Python.h>
+
+static PyObject* method_selam(PyObject* self, PyObject* args){
+    printf("Selam Ben Ümit ŞEN, umt_selam Fonksiyonu C Katmanında Başarılı Şekilde Çalıştı. :)))\n");
+    Py_RETURN_NONE;
+}
+
+static PyObject* method_selam2(PyObject* self, PyObject* Py_UNUSED(args)){
+    printf("Py_UNUSED ve METH_NOARGS yapısı / ilişkisi\n");
+    printf("Selam Ben Ümit ŞEN, umt_selam2 Fonksiyonu C Katmanında Başarılı Şekilde Çalıştı. :)))\n");
+    Py_RETURN_NONE;
+}
+
+/* video 4 */
+static PyObject* method_selam3(PyObject* self, PyObject* args){
+    const char* isim;
+    if(!PyArg_ParseTuple(args, "s", &isim)){
+        return NULL;
+    }
+    printf("Selam %s,artık C katmanındasın\n",isim);
+    Py_RETURN_NONE;
+}
+
+static PyMethodDef UmitMethods[] = {
+    {"umt_selam",method_selam,METH_VARARGS, "C dili özel selam."},
+    {"umt_selam2",method_selam2,METH_NOARGS, "umt_selam2 fonksiyonu help kısmı."},
+    {"umt_selam3",method_selam3,METH_VARARGS, "string parametreli selam 3 fonksiyonu."},
+    {NULL,NULL,0,NULL}
+};
+
+static struct PyModuleDef umit_core_module = {
+    PyModuleDef_HEAD_INIT,
+    "umit_core",
+    NULL,
+    -1,
+    UmitMethods
+};
+
+PyMODINIT_FUNC PyInit_umit_core(void){
+    return PyModule_Create(&umit_core_module);
+}
