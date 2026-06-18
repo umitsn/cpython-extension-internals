9b4f2945a1fd5047650ba43f93353dfc2fa2d782
Ümit Şen <49841290+umitsn@users.noreply.github.com>
2026-06-18 22:13:42 +0300

    Add files via upload

---
+++ lessons/005-integers-default-optional-parameters/main.py
@@ -0,0 +1,35 @@
+import umit_core as umit
+
+# print("umt_selam fonksiyonu")
+# umit.umt_selam()
+# umit.umt_selam("ümit")
+
+# # help(umit)
+
+# print("\numt_selam2 fonksiyonu")
+# umit.umt_selam2()
+
+# print("\numt_selam2 fonksiyonu")
+# umit.umt_selam2("ümit")
+
+## video 4
+# print("\numt_selam3 fonksiyonu")
+# umit.umt_selam3(["ümit"])
+"""
+umt_selam3 fonksiyonu
+Selam ümit,artık C katmanındasın
+"""
+
+# print("\numt_selam3 fonksiyonu")
+# umit.umt_selam3(123)
+"""
+umit.umt_selam3(123)
+TypeError: argument 1 must be str, not int
+"""
+
+## video 5
+n = 50
+a = umit.umt_selam4(n)
+print(f"Girilen Sayı : {n}")
+print(f"C Katmanından Dönen Toplam Sonucu : {a}")
+print(f"C Katmanından Dönen Toplam Sonucunun Tipi : {type(a)}")
---
+++ lessons/005-integers-default-optional-parameters/umit_core.c
@@ -0,0 +1,58 @@
+#include <Python.h>
+
+static PyObject* method_selam(PyObject* self, PyObject* args){
+    printf("Selam Ben Ümit ŞEN, umt_selam Fonksiyonu C Katmanında Başarılı Şekilde Çalıştı. :)))\n");
+    Py_RETURN_NONE;
+}
+
+static PyObject* method_selam2(PyObject* self, PyObject* Py_UNUSED(args)){
+    printf("Py_UNUSED ve METH_NOARGS yapısı / ilişkisi\n");
+    printf("Selam Ben Ümit ŞEN, umt_selam2 Fonksiyonu C Katmanında Başarılı Şekilde Çalıştı. :)))\n");
+    Py_RETURN_NONE;
+}
+
+/* video 4 */
+static PyObject* method_selam3(PyObject* self, PyObject* args){
+    const char* isim;
+    if(!PyArg_ParseTuple(args, "s", &isim)){
+        return NULL;
+    }
+    printf("Selam %s,artık C katmanındasın\n",isim);
+    Py_RETURN_NONE;
+}
+
+/* video 5 */
+static PyObject* method_selam4(PyObject* self, PyObject* args){
+    int n=10;
+    if(!PyArg_ParseTuple(args, "|i", &n)){
+        return NULL;
+    }
+
+    long tsum = 0;
+    for (int i = 1;i<=n;i++){
+        tsum +=i; 
+    }
+    return PyLong_FromLong(tsum);
+}
+
+
+
+static PyMethodDef UmitMethods[] = {
+    {"umt_selam",method_selam,METH_VARARGS, "C dili özel selam."},
+    {"umt_selam2",method_selam2,METH_NOARGS, "umt_selam2 fonksiyonu help kısmı."},
+    {"umt_selam3",method_selam3,METH_VARARGS, "string parametreli selam 3 fonksiyonu."},
+    {"umt_selam4",method_selam4,METH_VARARGS, "integer parametreli selam 4 fonksiyonu."},
+    {NULL,NULL,0,NULL}
+};
+
+static struct PyModuleDef umit_core_module = {
+    PyModuleDef_HEAD_INIT,
+    "umit_core",
+    NULL,
+    -1,
+    UmitMethods
+};
+
+PyMODINIT_FUNC PyInit_umit_core(void){
+    return PyModule_Create(&umit_core_module);
+}
